9604471f142747138442f45438a3e2ebcd268ae6
kyrylo <kyryloren@kyrylos-MBP.home>
2020-11-19 20:58:36 -0500

    Updated image

--- src/components/Layouts/Head.js
+++ src/components/Layouts/Head.js
@@ -3,9 +3,8 @@ import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 
 import config from '@utils/config';
-import twitterImage from '@images/favicon/demo.png';
 import favicon from '@images/favicon/favicon.ico';
-import ogImage from '@images/logos/avatar.png';
+import ogImage from '@images/screen.png';
 import favicon16x16 from '@images/favicon/favicon-16x16.png';
 import favicon32x32 from '@images/favicon/favicon-32x32.png';
 import appleTouchIcon from '@images/favicon/apple-touch-icon.png';
@@ -32,6 +31,7 @@ const Head = ({ metadata }) => {
       <meta property="og:url" content={metadata.siteUrl} />
       <meta property="og:site_name" content={metadata.title} />
       <meta itemProp="image" content={`${ogImage}`} />
+      <meta property='og:image' content={ogImage} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:image:type" content="image/png" />
@@ -43,7 +43,7 @@ const Head = ({ metadata }) => {
       <meta property="twitter:url" content={metadata.siteUrl} />
       <meta property="twitter:title" content={metadata.title} />
       <meta property="twitter:description" content={metadata.description} />
-      <meta property="twitter:image" content={twitterImage} />
+      <meta property="twitter:image" content={ogImage} />
 
       <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
       <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
